57418722eaea5488d590c45105136add928534d9
Chu Văn Nam <chunam47@gmail.com>
2023-04-17 00:12:21 +0700

    hot fix

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -60,10 +60,10 @@ const Layout: FC<ILayout> = ({
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
-          crossOrigin=""
+          crossOrigin="anonymous"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Quattrocento+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Quattrocento+Sans&display=swap"
           rel="stylesheet"
         />
 
